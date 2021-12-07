16255b9c8d86a1638280a00187e3ee7aedb227e4
Jose-Ribeir <jopirulim@gmail.com>
2021-12-07 18:01:54 +0000

    insert values in username field

--- app/src/main/java/com/example/myapplication/login/LoginViewModel.java
+++ app/src/main/java/com/example/myapplication/login/LoginViewModel.java
@@ -59,7 +59,7 @@ public class LoginViewModel extends ViewModel {
         if (username == null) {
             return false;
         }
-        if (username.contains("@")) {
+        if (!(username.contains("@") && username.contains("."))) {
             return Patterns.EMAIL_ADDRESS.matcher(username).matches();
         } else {
             return !username.trim().isEmpty();
