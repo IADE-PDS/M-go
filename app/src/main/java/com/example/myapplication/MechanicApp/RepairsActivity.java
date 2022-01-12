f1d5eda21f836c1fac95d94277279fdf2daa9591
Jonhot <jonhot111@gmail.com>
2022-01-12 15:18:54 +0000

    repair taken

--- app/src/main/java/com/example/myapplication/MechanicApp/RepairsActivity.java
+++ app/src/main/java/com/example/myapplication/MechanicApp/RepairsActivity.java
@@ -34,12 +34,19 @@ public class RepairsActivity extends AppCompatActivity {
 
     private void initializeMyListView()
     {
+
+        // 1 - Referenciar o componente do Design no Código
+        ListView myList = (ListView) findViewById(R.id.myListView);
+
+        // 2 - Criamos o tipo e o conjunto de objetos que vão ser listados
+        ArrayList<String> myItems = new ArrayList<String>();
+
         JSONArrayDownloader task = new JSONArrayDownloader();
         JSONArray objRepairs;
         JSONObject repair;
         try {
 
-            objRepairs=task.execute("http://mechanic-on-the-go.herokuapp.com/api/repairs/repairTaken").get();
+            objRepairs=task.execute("https://mechanic-on-the-go.herokuapp.com/api/repairs/repairTaken").get();
 
         } catch (ExecutionException | InterruptedException e) {
             e.printStackTrace();
@@ -49,22 +56,12 @@ public class RepairsActivity extends AppCompatActivity {
             for (int i = 0; i < objRepairs.length(); i++) {
                 try {
                     repair = objRepairs.getJSONObject(i);
+                    myItems.add(""+repair);
                 } catch (JSONException e) {
                     e.printStackTrace();
                 }
-
-
             }
         }
-        // 1 - Referenciar o componente do Design no Código
-        ListView myList = (ListView) findViewById(R.id.myListView);
-
-        // 2 - Criamos o tipo e o conjunto de objetos que vão ser listados
-        ArrayList<String> myItems = new ArrayList<String>();
-        myItems.add("Programação");
-        myItems.add("Aplicações para Dispositivos Móveis");
-        myItems.add("Computação Gráfica");
-        myItems.add("Programação Orientada a Objetos");
 
         // 3 - Criar um adaptador para passar a ArrayList para a ListView
         ArrayAdapter<String> myListAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, myItems);
@@ -74,6 +71,8 @@ public class RepairsActivity extends AppCompatActivity {
 
         // 6 - Chamamos a função que nos implementa o listener
         createListViewClickItemEvent(myList, myItems);
+
+
     }
 
     private void createListViewClickItemEvent(ListView list, final ArrayList<String> items)
